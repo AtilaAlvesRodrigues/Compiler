a45c889cc8893793f191d8d1347f33990f1653d6
Átila Alves <atilalves48@gmail.com>
2025-04-30 17:29:53 -0300

    Pacote helpers

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "helpers/vector.h"
+#include "helpers/buffer.h"
+
+int main() {
+    printf("Compiladores - TURMA A/B - GRUPO X\n");
+    return 0;
+
+    /* Teste das funcoes helpers */
+}
