fcd283ea84ef747b7fa312f7704d62b234ce2abe
Átila Alves <atilalves48@gmail.com>
2025-05-21 20:55:39 -0300

    Lab4 Finalizado

--- parser.c
+++ parser.c
@@ -217,21 +217,3 @@ node->exp.op = right_op;
 
 
 
-// ---------------------Texto original---------------------
-int parse_next(){ 
-    return 0;
-}
-
-int parse(struct compile_process* process) {                /*LAB3: Adicionar o prototipo no compiler.h */
-    struct node* node = NULL;
-    current_process = process;
-    
-    vector_set_peek_pointer(process->token_vec, 0);
-
-    while (parse_next() == 0) {
-        //node = node_peek();
-        vector_push(process->node_tree_vec, &node);
-    }
-    return PARSE_ALL_OK;
-}
-
